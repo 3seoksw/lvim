bb880a5a96c698732a2cd43fe66243221fac66cf
3seoksw <3suksw@gmail.com>
2023-01-02 17:09:59 +0900

    nvim-ts-autotag

--- config.lua
+++ config.lua
@@ -2,6 +2,6 @@ reload "KWS.core.options"
 reload "KWS.core.plugins"
 reload "KWS.core.keymaps"
 
-reload "KWS.plugins.treesitter"
+--reload "KWS.plugins.treesitter"
 reload "KWS.plugins.nvim-tree"
 reload "KWS.plugins.vimtex"
--- lua/KWS/core/plugins.lua
+++ lua/KWS/core/plugins.lua
@@ -1,15 +1,10 @@
 lvim.plugins = {
     {
-        "windwp/nvim-ts-autotag",
-        config = function()
-            require("nvim-ts-autotag").setup()
-        end,
-    },
-    {
         "iamcco/markdown-preview.nvim",
         run = function()
             vim.fn["mkdp#util#install"]()
         end,
     },
     { "lervag/vimtex" },
+    { "windwp/nvim-ts-autotag" },
 }
