e45fd42e262642b9b05e5884007f22cc2182bcb5
3seoksw <3suksw@gmail.com>
2023-01-02 17:06:25 +0900

    nvim-ts-autotag

--- lua/KWS/core/plugins.lua
+++ lua/KWS/core/plugins.lua
@@ -1,5 +1,11 @@
 lvim.plugins = {
     {
+        "windwp/nvim-ts-autotag",
+        config = function()
+            require("nvim-ts-autotag").setup()
+        end,
+    },
+    {
         "iamcco/markdown-preview.nvim",
         run = function()
             vim.fn["mkdp#util#install"]()
