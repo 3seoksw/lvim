f6f6364b4eb6b7c8f0afb82372125c85e7c666a7
3seoksw <3suksw@gmail.com>
2023-03-08 23:35:55 +0900

    vimtex concealment modified

--- lua/KWS/plugins/vimtex.lua
+++ lua/KWS/plugins/vimtex.lua
@@ -1,7 +1,14 @@
 vim.cmd[["filetype plugin indent on"]]
-vim.g.tex_flavor = "latex"
-vim.g.vimtex_view_method = "skim"
-vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
-vim.g.vimtex_quickfix_mod = 0
-vim.cmd[["set conceallevel = 2"]]
-vim.g.tex_conceal = "abdmg"
+vim.cmd[[
+let g:tex_flavor='latex'
+let g:vimtex_view_method='skim'
+let g:vimtex_quickfix_mode=0
+set conceallevel=1
+let g:tex_conceal='abdmg'
+]]
+--vim.g.tex_flavor = "latex"
+--vim.g.vimtex_view_method = "skim"
+--vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+--vim.g.vimtex_quickfix_mod = 0
+--vim.cmd[["set conceallevel = 2"]]
+--vim.g.tex_conceal = "abdmg"
