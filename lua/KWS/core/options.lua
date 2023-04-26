921041b2b4db3a94b0210a0370e2e79d5d55c410
WooSeok Kim <3suksw@gmail.com>
2023-04-26 22:13:53 +0900

    change of colourscheme: tokyonight

--- lua/KWS/core/options.lua
+++ lua/KWS/core/options.lua
@@ -1,27 +1,28 @@
+lvim.colorscheme = "tokyonight"
 local options = {
-	number = true,			-- enable line number
-	relativenumber = true,	-- enable relative line number
-	mouse = "a",			-- allow the mouse to be used
-	cursorline = true,		-- highlight the current line
-	smartcase = true,
-	smartindent = true, 	-- make indenting smarter
-	tabstop = 4,
-	shiftwidth = 4,
-	softtabstop = 0,
-	errorbells = true,
-	incsearch = true,
-	scrolloff = 8,
-	termguicolors = true,
-	hlsearch = true,
+    number = true, -- enable line number
+    relativenumber = true, -- enable relative line number
+    mouse = "a", -- allow the mouse to be used
+    cursorline = true, -- highlight the current line
+    smartcase = true,
+    smartindent = true, -- make indenting smarter
+    tabstop = 4,
+    shiftwidth = 4,
+    softtabstop = 0,
+    errorbells = true,
+    incsearch = true,
+    scrolloff = 8,
+    termguicolors = true,
+    hlsearch = true,
 }
 
 vim.opt.shortmess:append "c"
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+    vim.opt[k] = v
 end
 
-vim.cmd ("set clipboard=unnamed")
-vim.cmd ("set whichwrap+=<,>,[,],h,l")
-vim.cmd ("set iskeyword+=-")
-vim.cmd ("set formatoptions-=cro") -- TODO: this doesn't seem to work
+vim.cmd("set clipboard=unnamed")
+vim.cmd("set whichwrap+=<,>,[,],h,l")
+vim.cmd("set iskeyword+=-")
+vim.cmd("set formatoptions-=cro") -- TODO: this doesn't seem to work
